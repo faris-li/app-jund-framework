8d42f09f1a6310566874ff431bd2c8ba0d1442f9
zhijund <762718879@qq.com>
2017-09-02 18:31:20 +0800

    提交基础框架代码

---
+++ app-cloud-framework-testcase/src/main/java/com/jund/framework/testcase/TestaCase.java
@@ -0,0 +1,16 @@
+package com.jund.framework.testcase;
+
+/**
+ * Created by zhijund on 2017/9/2.
+ */
+public abstract class TestaCase {
+
+    abstract void testPage() throws Exception;
+
+    abstract void testFindOne() throws Exception;
+
+    abstract void testSave() throws Exception;
+
+    abstract void testDelete() throws Exception;
+
+}
---
+++ app-cloud-framework-testcase/src/main/java/com/jund/framework/testcase/suite/WebSuite.java
@@ -0,0 +1,7 @@
+package com.jund.framework.testcase.suite;
+
+/**
+ * Created by zhijund on 2017/9/2.
+ */
+public class WebSuite extends BaseSuite{
+}
