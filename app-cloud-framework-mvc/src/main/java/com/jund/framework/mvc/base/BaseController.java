84e5b406d6f01a681281265cc737b1637ffb1fd2
zhijund <762718879@qq.com>
2017-09-02 18:26:02 +0800

    提交基础框架代码

---
+++ app-cloud-framework-mvc/src/main/java/com/jund/framework/mvc/base/BaseController.java
@@ -0,0 +1,7 @@
+package com.jund.framework.mvc.base;
+
+/**
+ * Created by zhijund on 2017/9/2.
+ */
+public class BaseController {
+}
---
+++ app-cloud-framework-mvc/src/main/java/com/jund/framework/mvc/filter/CORSFilter.java
@@ -0,0 +1,7 @@
+package com.jund.framework.mvc.filter;
+
+/**
+ * Created by zhijund on 2017/9/2.
+ */
+public class CORSFilter {
+}
