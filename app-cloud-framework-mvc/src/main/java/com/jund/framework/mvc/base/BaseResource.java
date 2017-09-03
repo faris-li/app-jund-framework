b31bdfe27f1e09a20ebf5d278442f9eb67dd8bd4
zhijund <762718879@qq.com>
2017-09-03 10:14:29 +0800

    提交基础框架代码

---
+++ app-cloud-framework-mvc/src/main/java/com/jund/framework/mvc/base/BaseResource.java
@@ -0,0 +1,11 @@
+package com.jund.framework.mvc.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by zhijund on 2017/9/2.
+ */
+public abstract class BaseResource {
+    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
+}
