d6fcca95b2f79506f6d36a0327675d579ec0ac7f
zhijund <lfners_1987>
2017-09-06 16:39:29 +0800

    更新基础框架代码

--- app-cloud-framework-testcase/src/main/java/com/jund/framework/testcase/suite/BaseSuite.java
+++ app-cloud-framework-testcase/src/main/java/com/jund/framework/testcase/suite/BaseSuite.java
@@ -1,7 +1,20 @@
 package com.jund.framework.testcase.suite;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by zhijund on 2017/9/2.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
 public abstract class BaseSuite {
+
+    public ObjectMapper mapper = new ObjectMapper();
+
+    public String toJsonString(Object target) throws Exception{
+        return mapper.writeValueAsString(target);
+    }
+
+    public abstract void testBaseCase();
 }
--- app-cloud-framework-testcase/src/main/java/com/jund/framework/testcase/suite/WebSuite.java
+++ app-cloud-framework-testcase/src/main/java/com/jund/framework/testcase/suite/WebSuite.java
@@ -1,7 +1,32 @@
 package com.jund.framework.testcase.suite;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 /**
  * Created by zhijund on 2017/9/2.
  */
+@WebAppConfiguration
 public class WebSuite extends BaseSuite{
+
+    public String domainUrl;
+
+    public MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setupMockMvc(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void testBaseCase(){
+    }
 }
