20b7dbd24125b503dfd3830581fac47b3568d466
zhijund <762718879@qq.com>
2017-09-23 21:10:59 +0800

    提交代码

---
+++ app-jund-framework-core/src/main/java/com/jund/framework/core/Const.java
@@ -0,0 +1,15 @@
+package com.jund.framework.core;
+
+/**
+ * Created by zhijund on 2017/9/9.
+ */
+public class Const {
+
+    public static final class DICT{
+
+        public static final int INTEGER_FALSE = 0;
+        public static final int INTEGER_TRUE= 1;
+
+    }
+
+}
